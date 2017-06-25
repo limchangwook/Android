ab0278438345cb7cef9ee4a405b246204e1f36ca
user <seexygirl@naver.com>
2017-06-25 18:34:48 +0900

    캐릭터 클릭으로 움직이게하기

--- app/src/main/java/org/techtown/mission12/MainActivity.java
+++ app/src/main/java/org/techtown/mission12/MainActivity.java
@@ -1,14 +1,9 @@
 package org.techtown.mission12;
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.support.annotation.Nullable;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
