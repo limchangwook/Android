75889b8460cb718f1058d1ebd03581e898026491
user <seexygirl@naver.com>
2017-06-25 18:28:18 +0900

    캐릭터 클릭으로 움직이게하기

--- app/src/main/java/org/techtown/mission12/MainActivity.java
+++ app/src/main/java/org/techtown/mission12/MainActivity.java
@@ -1,7 +1,14 @@
 package org.techtown.mission12;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
 
--- app/src/main/java/org/techtown/mission12/TouchBoard.java
+++ app/src/main/java/org/techtown/mission12/TouchBoard.java
@@ -1,13 +1,13 @@
 package org.techtown.mission12;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.support.annotation.Nullable;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Paint;
+        import android.support.annotation.Nullable;
+        import android.util.AttributeSet;
+        import android.view.MotionEvent;
+        import android.view.View;
 
 /**
  *
